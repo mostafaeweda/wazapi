41221e26547cfe81aa18ab03bc35ea0102530ac8
Mostafa Eweda <mostafa.eweda17@gmail.com>
2012-04-21 20:21:40 +0200

    Fix the comment shape

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,9 @@ var app = require('../app');
 
 var User = app.Schema.User;
 
-// Create a user
-
-exports.create = function(req, res) {
+/*
+ * Create a user
+ */
+ exports.create = function(req, res) {
   res.render('users/create', { title: 'Express' });
 };
