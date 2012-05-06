8d721ec700d6d23b29f6a5f1ed7bb4b256ec5a8e
Ahmed <ahmedelmorsy89@gmail.com>
2012-05-06 17:38:58 +0200

    added book details to book object

--- db/schema.js
+++ db/schema.js
@@ -8,4 +8,3 @@ exports.User     = require('./user');
 exports.Instance = require('./instance');
 exports.Rental   = require('./rental');
 exports.Purchase = require('./purchase');
-exports.BookDetails = require('./book_details');
